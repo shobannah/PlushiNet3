ece28ab29b9b67e3126fa7f29eb71166586d3b7c
shobannah <116910257+shobannah@users.noreply.github.com>
2023-04-28 18:25:43 -0400

    updatad connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/mern-shopping');
+mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/plushinetdb');
 
 module.exports = mongoose.connection;
 
